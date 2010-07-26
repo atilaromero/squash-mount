f320aaccd023c6695224558f4a4c43b04ea2c9bf
Atila Romero <atilaromero@gmail.com>
2010-07-26 15:11:30 -0300

    unclassing consumer
    	modified:   queue/consumer.py

--- queue/consumer.py
+++ queue/consumer.py
@@ -5,34 +5,32 @@ import amqplib.client_0_8 as amqpclient
 def recv_callback(msg):
     print 'Received: ' + msg.body
 
-class Consumer:
-    def __ini__(self):
-        conn=amqpclient.Connection(host='localhost:5672',
-                                   userid='guest',
-                                   password='guest',
-                                   virtual_host='/',
-                                   insist=False)
-        chan=conn.channel()
-
-        chan.queue_declare(queue='po_box',
-                           durable=False,
-                           exclusive=False, auto_delete=False)
-
-        chan.exchange_declare(exchange="sorting_room",
-                              type="direct",
-                              durable=False,
-                              auto_delete=False,)
-
-        chan.queue_bind(exchange="sorting_room",
-                        routing_key="jason",
-                        queue="po_box")
-
-    def consume(self):
-        chan.basic_consume(queue='po_box',
-                           no_ack=True,
-                           callback=recv_callback,
-                           consumer_tag="testtag")
-        while True:
-            chan.wait()
-
+def consume():
+    conn=amqpclient.Connection(host='localhost:5672',
+                               userid='guest',
+                               password='guest',
+                               virtual_host='/',
+                               insist=False)
+    chan=conn.channel()
+    
+    chan.queue_declare(queue='po_box',
+                       durable=False,
+                       exclusive=False, auto_delete=False)
+    
+    chan.exchange_declare(exchange="sorting_room",
+                          type="direct",
+                          durable=False,
+                          auto_delete=False,)
+    
+    chan.queue_bind(exchange="sorting_room",
+                    routing_key="jason",
+                    queue="po_box")
+    
+    chan.basic_consume(queue='po_box',
+                       no_ack=True,
+                       callback=recv_callback,
+                       consumer_tag="testtag")
+    while True:
+        chan.wait()
+    
 #chan.basic_cancel("testtag")
