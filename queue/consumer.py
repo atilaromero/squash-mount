0f3921fa628610f2adfd837b42ae7baf96abe78e
Atila Romero <atilaromero@gmail.com>
2010-07-26 16:46:08 -0300

    using class in consumer

--- queue/consumer.py
+++ queue/consumer.py
@@ -2,35 +2,46 @@
 
 import amqplib.client_0_8 as amqpclient
 
-def recv_callback(msg):
-    print 'Received: ' + msg.body
+class Consumer:
+    def __init__(self):
+        self.conn=amqpclient.Connection(host='localhost:5672',
+#                                   userid='guest',
+#                                   password='guest',
+#                                   virtual_host='/',
+                                   insist=False)
+        self.chan=self.conn.channel()
+    
+    def create_queue(self):
+        self.chan.queue_declare(queue='po_box',
+                           durable=False,
+                           exclusive=False,
+                           auto_delete=False)
+        
+        self.chan.exchange_declare(exchange="sorting_room",
+                              type="direct",
+                              durable=False,
+                              auto_delete=False,)
+        
+        self.chan.queue_bind(exchange="sorting_room",
+                             routing_key="jason",
+                             queue="po_box")
+        
 
-def consume():
-    conn=amqpclient.Connection(host='localhost:5672',
-                               userid='guest',
-                               password='guest',
-                               virtual_host='/',
-                               insist=False)
-    chan=conn.channel()
-    
-    chan.queue_declare(queue='po_box',
-                       durable=False,
-                       exclusive=False, auto_delete=False)
-    
-    chan.exchange_declare(exchange="sorting_room",
-                          type="direct",
-                          durable=False,
-                          auto_delete=False,)
-    
-    chan.queue_bind(exchange="sorting_room",
-                    routing_key="jason",
-                    queue="po_box")
-    
-    chan.basic_consume(queue='po_box',
-                       no_ack=True,
-                       callback=recv_callback,
-                       consumer_tag="testtag")
-    while True:
-        chan.wait()
-    
-#chan.basic_cancel("testtag")
+    def recv_callback(self,msg):
+        print 'Received: ' + msg.body
+        self.chan.basic_ack(msg.delivery_tag)
+
+    def consume(self):
+        self.chan.basic_consume(queue='po_box',
+                                callback=self.recv_callback,
+                                consumer_tag="testtag")
+        try:
+            while True:
+                self.chan.wait()
+        finally:
+            self.chan.basic_cancel("testtag")
+
+    def __del__(self):
+        self.chan.close()
+        self.conn.close()
+
